b8ef09f22d919a2ea78cb13dcab534fb3af7dc23
360Sch <on9commerce@gmail.com>
2024-04-05 01:19:05 +0800

    Create folder structure

--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
+// 1. Logo, navigation bar, login/signup
+// 2. Hero
+//     1. Image
+//     2. Video
+// 3. As Featured in
+// 4. How it works
+// 5. Key Features (how it benefit you, what is the benefit)
+// 6. List of Features(card)(full features)
+// 7. FAQs
+// 8. CTA
+// 9. Footer
+// 10. 404 pag
 // import Image from "next/image";
-
+import Hero from "@/app/_components/Hero";
 export default function Home() {
-  return <>Home page</>;
+  return (
+    <>
+      <Hero />
+      Home page
+    </>
+  );
 }
